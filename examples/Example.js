7de4c6bca6a3d66acb358a883589df6d003b44d7
Pankaj Bat <pankaj.bat@lenskart.in>
2019-10-29 23:20:17 +0530

    configured multiple slide

--- examples/Example.js
+++ examples/Example.js
@@ -7,11 +7,32 @@ function Example() {
     return (
         <div className="example">
             <div className="header">Light weight react carousel library with ssr support.</div>
-            <Carousel height="480px" width="640px">
+            <div className="header">Carousel with single slide</div>
+            <Carousel height="480" width="608" slidesInView="1">
                 <img src="/assets/image_1.jpg" />
                 <img src="/assets/image_2.jpeg" />
                 <img src="/assets/image_1.jpg" />
                 <img src="/assets/image_2.jpeg" />
+                <img src="/assets/image_1.jpg" />
+                <img src="/assets/image_1.jpg" />
+            </Carousel>
+            <div className="header">Carousel with multiple slide</div>
+            <Carousel height="200" width="610" slidesInView="3">
+                <img src="/assets/model1.jpg" />
+                <img src="/assets/model2.jpg" />
+                <img src="/assets/model3.jpg" />
+                <img src="/assets/model4.jpg" />
+                <img src="/assets/model5.jpg" />
+                <img src="/assets/model7.jpg" />
+                <img src="/assets/model8.jpg" />
+            </Carousel>
+            <div className="header">Carousel with no default width</div>
+            <Carousel height="480" slidesInView="2">
+                <img src="/assets/image_1.jpg" />
+                <img src="/assets/image_2.jpeg" />
+                <img src="/assets/image_1.jpg" />
+                <img src="/assets/image_2.jpeg" />
+                <img src="/assets/image_1.jpg" />
             </Carousel>
         </div>
     )
--- src/Carousel.js
+++ src/Carousel.js
@@ -10,19 +10,17 @@ function Carousel(props) {
     const [activeSlide, setActiveSlide] = useState(0);
 
     // forced width
-    let w = parseInt(props.width);
-    if (isNaN(w)) w = 0;
-    const [slideWidth, setSlideWidth] = useState(w);
+    const [slideWidth, setSlideWidth] = useState(props.width / props.slidesInView);
 
     useEffect(() => {
-        if (props.width != null || props.width !== '') return;
+        if (props.width != null) return;
 
         const $container = containerRef.current;
         if ($container) {
             const { width } = $container.getBoundingClientRect();
-            setSlideWidth(width);
+            setSlideWidth(width / props.slidesInView);
         }
-    });
+    }, []);
 
     const previous = () => {
         const slide = activeSlide - 1;
@@ -31,24 +29,33 @@ function Carousel(props) {
     }
 
     const next = () => {
+        const count = props.slidesInView - 1;
         const slide = activeSlide + 1;
-        if (slide >= slidesCount) return;
+        if (slide >= slidesCount - count) return;
         setActiveSlide(slide);
     }
 
+    const getDots = (count) => {
+        const dots = [];
+        for (let i = 0; i < count; i++) {
+            dots.push(<span className={`nav-dot ${activeSlide === i ? 'active' : ''}`} key={i}></span>);
+        }
+        return dots;
+    }
+
     return (
-        <div className="carousel" style={{ width: props.width }}>
+        <div className="carousel" style={{ width: `${props.width}${props.unit}` }}>
             <button onClick={previous} type="button" className="carousel-btn previous" aria-label="carousel-prev" />
             <Swipe previous={previous} next={next}>
-                <div ref={containerRef} className="carousel-track-container" style={{ height: props.height }}>
-                    <div className="carousel-track" style={{ transform: `translateX(-${slideWidth * activeSlide}px)` }}>
+                <div ref={containerRef} className="carousel-track-container" style={{ height: `${props.height}${props.unit}` }}>
+                    <div className="carousel-track" style={{ transform: `translateX(-${slideWidth * activeSlide}${props.unit})` }}>
                         {React.Children.map(props.children, (node, index) => <div className={`carousel-slide ${activeSlide === index ? 'active-slide' : ''}`} style={{ left: index * slideWidth }} key={index}>{node}</div>)}
                     </div>
                 </div>
             </Swipe>
             <button onClick={next} type="button" className="carousel-btn next" aria-label="carousel-next" />
             <div className="nav-dots-container">
-                {React.Children.map(props.children, (node, index) => <span className={`nav-dot ${activeSlide === index ? 'active' : ''}`} key={index}></span>)}
+                {getDots(slidesCount - (props.slidesInView - 1))}
             </div>
         </div>
     );
@@ -58,5 +65,7 @@ export default Carousel;
 
 Carousel.defaultProps = {
     width: undefined,
-    height: ''
+    height: undefined,
+    unit: 'px',
+    slidesInView: 1
 }
